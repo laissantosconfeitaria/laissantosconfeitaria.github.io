c229fb2b593a2005f243604b0d4c41707a152fee
laissantosconfeitaria <docerialaissantos@gmail.com>
2026-05-03 01:20:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ls-gestao-v13';
+const CACHE = 'ls-gestao-v14';
 const ASSETS = [
   'https://laissantosconfeitaria.github.io/gestao.html',
   'https://laissantosconfeitaria.github.io/icon-192.png',
